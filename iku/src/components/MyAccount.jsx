73f9b2016dcd1c2c059c61d53af61b836390154b
Cinodevv <96893418+Cinodevv@users.noreply.github.com>
2022-10-01 21:40:43 -0400

    feat: create myaccount component
    - design myAccount page
    - create myaccount component
    - change img file names to match convention
    - add default user img

---
+++ iku/src/components/MyAccount.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "bulma/css/bulma.css";
+import tempUserImg from "./../assets/default_user.jpg";
+import styles from "./../styles/cssAccount.module.css";
+
+export default function MyAccount() {
+  return (
+    <>
+      <div>
+        <div>
+          <section>
+            <div>
+              <p class={styles.pageText}>My Account</p>
+            </div>
+            <div class="box has-text-centered">
+              <img
+                class="is-rounded"
+                src={tempUserImg}
+                height="100px"
+                width="100px"
+              ></img>
+              <p class={styles.pageText}>Username</p>
+              <p class={styles.pageText}>Email</p>
+              <p class={styles.pageText}>Location</p>
+              <button class={styles.acctBtns}>Change Profile Picture</button>
+              <br></br>
+              <br></br>
+              <button class={styles.acctBtns}>Edit Account Information</button>
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+}
