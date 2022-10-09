229f7461582712a14854ae983722f26c19656d6d
Cinodevv <96893418+Cinodevv@users.noreply.github.com>
2022-10-09 09:48:12 -0400

    fix: clean appjs for merge
    - Remove code so to avoid conficts in main merge

--- iku/src/App.js
+++ iku/src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./App.css";
-import AccountPage from "./pages/Accountpage";
-import Homepage from "./pages/Homepage";
 
-function App() {
-  return <AccountPage />;
-}
+function App() {}
 
 export default App;
