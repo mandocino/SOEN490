3dec044ea52e86372728645152f4c22fd876c0f0
FrederickMunro <fredmunro7@gmail.com>
2022-10-15 16:22:11 -0400

    Fixed my files

--- iku/src/components/Login/LoginForm.jsx
+++ iku/src/components/Login/LoginForm.jsx
@@ -1,75 +1,75 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 // import axios from 'axios';
 
-import "../../styles/Login.css";
+import '../../styles/Login.css';
 
-import LoginInput from "./LoginInput";
+import LoginInput from './LoginInput'
 
 const LoginForm = ({ inputs, type }) => {
-  const [inputValue, setInputValues] = useState(inputs);
 
-  const _handleOnChange = (event, count) => {
-    console.log(count);
-    let newValue = inputValue.slice();
-    newValue[count].value = event.target.value;
-    setInputValues(newValue);
-  };
+    const [inputValue, setInputValues] = useState(inputs);
 
-  const getButtonType = (type) => {
-    return type === 0 ? "Register" : "Log in";
-  };
-
-  const getRedirectMessage = (type) => {
-    return type === 0
-      ? "Already have an account? Sign in."
-      : "Create an account";
-  };
-
-  const getRedirectLink = (type) => {
-    return type === 0 ? "/login" : "/register";
-  };
-
-  const testInput = async () => {
-    switch (type) {
-      case 0:
-        // Register information here.
-        break;
-      case 1:
-        // Login information here.
-        // To get value of a certain input field use --> inputValue[i].value ||| where i is the 'i'th input field on the webpage.
-        //const res = await axios.post(`http://localhost:5000/user`, {username: inputValue[0].value, password: inputValue[1].value})
-        break;
-      default:
+    const _handleOnChange = (event, count) => {
+        console.log(count);
+        let newValue = inputValue.slice();
+        newValue[count].value = event.target.value;
+        setInputValues(newValue);
     }
-  };
 
-  let count = 0;
+    const getButtonType = (type) => {
+        return type === 0 ? 'Register' : 'Log in';
+    }
 
-  return (
-    <form id="login-form">
-      {inputValue.map(({ label, type, name, ariaLabel, value }) => {
-        return (
-          <LoginInput
-            label={label}
-            type={type}
-            name={name}
-            ariaLabel={ariaLabel}
-            value={value}
-            handleInputChange={_handleOnChange}
-            count={count++}
-            key={name}
-          />
-        );
-      })}
-      <div id="button-container">
-        <button id="login-button" onClick={testInput}>
-          {getButtonType(type)}
-        </button>
-        <Link to={getRedirectLink(type)}>{getRedirectMessage(type)}</Link>
-      </div>
-    </form>
-  );
-};
+    const getRedirectMessage = (type) => {
+        return type === 0 ? 'Already have an account? Log in' : 'Create an account';
+    }
 
-export default LoginForm;
+    const getRedirectLink = (type) => {
+        return type === 0 ? '/login' : '/register';
+    }
+
+    const testInput = async () => {
+        switch (type) {
+            case 0:
+                // Register information here.
+                break;
+            case 1:
+                // Login information here.
+                // To get value of a certain input field use --> inputValue[i].value ||| where i is the 'i'th input field on the webpage.
+                //const res = await axios.post(`http://localhost:5000/user`, {username: inputValue[0].value, password: inputValue[1].value})
+                break;
+            default:
+
+        }
+    }
+
+    let count = 0;
+
+    return(
+        <form id='login-form'>
+            {
+                inputValue.map(({ label, type, name, ariaLabel, value }) => {
+                    return (
+                        <LoginInput
+                            label={label}
+                            type={type}
+                            name={name}
+                            ariaLabel={ariaLabel}
+                            value={value}
+                            handleInputChange={_handleOnChange}
+                            count={count++}
+                            key={name}
+                        />
+                    )
+                })
+            }
+            <div id='button-container'>
+                <button id='login-button' onClick={testInput}>{getButtonType(type)}</button>
+                <Link to={getRedirectLink(type)}>{getRedirectMessage(type)}</Link>
+            </div>
+        </form>
+    )
+}
+
+export default LoginForm
--- iku/src/components/Login/LoginLogo.jsx
+++ iku/src/components/Login/LoginLogo.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 
-import "../../styles/Login.css";
+import '../../styles/Login.css';
 
-import Logo from "../../assets/iku_logo_white.png";
-import { Link } from "react-router-dom";
+import Logo from '../../assets/iku_logo_white.png'
 
 const LoginLogo = () => {
-  return (
-    <>
-      <img id="login-logo" src={Logo} alt="Iku logo" />
-      <h2 id="login-title">
-        Let's find your
-        <br />
-        transit scores
-      </h2>
-      <br />
-      <Link to="/">
-        <button id="login-button">Return to Homepage</button>
-      </Link>
-    </>
-  );
-};
+    return(
+        <>
+            <img id='login-logo' src={Logo} alt='Iku logo' />
+            <h2 id='login-title'>Let's find your<br />transit scores</h2>
+        </>
+    );
+}
 
-export default LoginLogo;
+export default LoginLogo
