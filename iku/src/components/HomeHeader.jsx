d68b03a0f70a8818f6747e62bca32bf88f8e4c3e
srauph <47877347+srauph@users.noreply.github.com>
2022-11-04 01:57:08 -0400

    continued work

--- iku/src/components/HomeHeader.jsx
+++ iku/src/components/HomeHeader.jsx
@@ -32,7 +32,7 @@ export default function HomeHeader() {
                     <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                       <svg aria-hidden="true" class="w-5 h-5 text-emerald-100" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                     </div>
-                    <input type="search" id="default-search" class="transition ease-in-out duration-200 block p-4 pl-10 w-full text-md font-semibold text-white placeholder-white bg-emerald-500 rounded-lg border-2 border-emerald-200 dark:border-emerald-300 focus:ring-green-300 focus:border-blue-500 dark:bg-emerald-700 dark:placeholder-emerald-100 dark:focus:ring-green-300 dark:focus:border-blue-500" placeholder="Search Location" required/>
+                    <input type="search" id="default-search" class="transition ease-in-out duration-200 block p-4 pl-10 w-full text-md font-semibold text-white placeholder-white bg-emerald-500 rounded-lg border-2 border-emerald-200 dark:border-emerald-300 focus:ring-green-300 dark:bg-emerald-700 dark:placeholder-emerald-100 dark:focus:ring-green-300" placeholder="Search Location" required/>
                     <button type="submit" class="transition ease-in-out duration-200 text-emerald-600 dark:text-emerald-800 absolute right-3 bottom-3 bg-emerald-200 hover:bg-emerald-300 focus:ring-4 focus:outline-none focus:ring-green-300 font-semibold rounded-lg text-sm px-4 py-2 dark:bg-emerald-300 dark:hover:bg-emerald-500 dark:focus:ring-green-300">Search</button>
                   </div>
                 </form>
--- iku/src/components/SearchBar.jsx
+++ iku/src/components/SearchBar.jsx
@@ -17,8 +17,8 @@ export default function SearchBar() {
             <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
               <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             </div>
-            <input type="search" id="default-search" class="transition ease-in-out duration-200 block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border-2 border-emerald-500 focus:ring-green-300 focus:border-blue-500 dark:bg-gray-700 dark:border-emerald-400 dark:placeholder-gray-400 dark:text-white dark:focus:green-300 dark:focus:border-blue-500" placeholder="Search Location" required/>
-            <button type="submit" class="transition ease-in-out duration-200 text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-500 dark:focus:ring-green-300">Search</button>
+            <input type="search" id="default-search" class="accent-white transition ease-in-out duration-200 block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border-2 border-emerald-500 focus:ring-green-300 focus:border-emerald-700 dark:bg-gray-700 dark:border-emerald-400 dark:placeholder-gray-400 dark:text-white dark:focus:border-white" placeholder="Search Location" required/>
+            <button type="submit" class="transition ease-in-out duration-200 text-white absolute right-2.5 bottom-2.5 bg-emerald-500 hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-semibold rounded-lg text-sm px-4 py-2 dark:bg-emerald-400 dark:hover:bg-emerald-600 dark:focus:ring-green-300">Search</button>
           </div>
         </form>
       </div>
