d2ffebdc1d8a970265c2f347ee48f3efe1772df5
Cinodevv <96893418+Cinodevv@users.noreply.github.com>
2022-11-04 00:35:25 -0400

    Change Accountpage to Tailwind
    Remove css and bulma components for accountpage.

--- iku/src/components/MyAccount.jsx
+++ iku/src/components/MyAccount.jsx
@@ -1,40 +1,28 @@
 import React from "react";
 import tempUserImg from "./../assets/default_user.jpg";
 import styles from "./../styles/cssAccount.module.css";
+import LinkButton from "./custom/LinkButton";
 
 export default function MyAccount() {
   return (
     <>
-      <div>
+      <div class="bg-emerald-500 mx-auto text-center rounded-3xl text-white max-w-xl mt-20">
         <div>
-          <section class="hero">
-            <div class={styles.container}>
-              <div>
-                <p class={styles.pageTitle}>My Account</p>
-              </div>
-              <div>
-                <div class={styles.accountBox}>
-                  <img
-                    class="is-rounded"
-                    src={tempUserImg}
-                    height="100px"
-                    width="100px"
-                  ></img>
-                  <p class={styles.pageText}>Username</p>
-                  <p class={styles.pageText}>Email</p>
-                  <p class={styles.pageText}>Location</p>
-                  <button class={styles.acctBtns}>
-                    Change Profile Picture
-                  </button>
-                  <br></br>
-                  <br></br>
-                  <button class={styles.acctBtns}>
-                    Edit Account Information
-                  </button>
-                </div>
-              </div>
-            </div>
-          </section>
+          <p class="pt-7 pb-7 text-2xl">My Account</p>
+          <img
+            class="w-20 rounded-lg mx-auto pb-7"
+            src={tempUserImg}
+            alt="tempUserImg"
+          ></img>{" "}
+          <p class="pb-4">Username: </p>
+          <p class="pb-4">Email: </p>
+          <p class="pb-4">Location:</p>
+          <div class="w-60 mx-auto pb-10">
+            <LinkButton class="mb-10" to="/dashboard">
+              Change Profile Picture
+            </LinkButton>
+            <LinkButton to="/dashboard">Edit Account Information</LinkButton>
+          </div>
         </div>
       </div>
     </>
