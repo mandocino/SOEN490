6d39b2a66fcdb43b1402b4873c76ae0a0256e7a0
Tommy Soucy <soucy.tommy@gmail.com>
2022-11-04 14:26:07 -0400

    Fixed backend test

--- iku/src/tests/User.test.js
+++ iku/src/tests/User.test.js
@@ -10,15 +10,23 @@ describe("Default test", () => {
 
 // USER
 describe("User tests", () => {
-    test("Get all users", () => {
-        axios.get('http://localhost:5000/users')
-            .then(function (response) {
-                expect(Array.isArray(response.data)).toBe(true)
-                expect(response.data.length).toBeGreaterThan(0);
-                expect(response.data[0]).toHaveProperty('_id');
-            })
-            .catch(function (error) {
-                throw new Error('Failed to get users')
-            });
+    test("Get all users", async () => {
+        const res = await axios.get('http://localhost:5000/users');
+        expect(Array.isArray(res.data)).toBe(true)
+        expect(res.data.length).toBeGreaterThan(0);
+        expect(res.data[0]).toHaveProperty('_id');
+    });
+
+    test("Get user by ID", async () => {
+        const res = await axios.get('http://localhost:5000/users');
+        expect(Array.isArray(res.data)).toBe(true)
+        expect(res.data.length).toBeGreaterThan(0);
+        expect(res.data[0]).toHaveProperty('_id');
+
+        const resID = await axios.get(`http://localhost:5000/userByID/${res.data[0]._id}`);
+        expect(Array.isArray(resID.data)).toBe(true)
+        expect(resID.data.length).toBeGreaterThan(0);
+        expect(resID.data[0]).toHaveProperty('email');
+        expect(resID.data[0].email).toEqual(res.data[0].email);
     });
 });
