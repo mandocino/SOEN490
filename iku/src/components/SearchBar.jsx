aa6c96f79eff575715a71fece9fb3fe7e02db06e
FrederickMunro <fredmunro7@gmail.com>
2022-11-05 15:11:53 -0400

    Implemented datalist to searchbar for location suggestions

--- iku/src/components/SearchBar.jsx
+++ iku/src/components/SearchBar.jsx
@@ -92,7 +92,7 @@ export default function SearchBar() {
   return (
     <>
         <div class="shadow-lg shadow-gray-400 dark:shadow-gray-900 rounded-3xl w-full bg-cover bg-center bg-[url('/src/assets/stm_bus.jpg')]">
-          <div class="rounded-3xl p-2 xs:p-8 sm:p-16 w-full backdrop-blur backdrop-brightness-50 flex items-center justify-center">
+          <div class="rounded-3xl p-2 xs:p-8 sm:p-16 w-full backdrop-blur backdrop-brightness-50 flex flex-col items-center justify-center">
             <div class="text-emerald-400 dark:text-emerald-400 flex flex-col gap-4 max-w-screen-md p-4">
               <p class="text-center text-5xl text-transparent bg-clip-text bg-gradient-to-r from-emerald-200 to-emerald-500">
                 Let's find your transit scores
@@ -112,18 +112,21 @@ export default function SearchBar() {
                       id="default-search"
                       class="accent-emerald-700 dark:accent-white transition ease-in-out duration-200 block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border-2 border-emerald-500 focus:border-emerald-700 dark:bg-gray-700 dark:border-emerald-400 dark:placeholder-gray-400 dark:text-white dark:focus:border-white"
                       placeholder="Search Location"
+                      list="suggestions"
+                      autocomplete="off"
                       required
                     />
-                    <div>
+                    <datalist id="suggestions">
                       {suggestions.map(suggestion => {
                         return(
-                          <div 
-                            onClick={() => selectSuggestion(suggestion)}>
-                            {suggestion}
-                          </div>
+                          <option
+                            className="cursor-pointer"
+                            onClick={() => selectSuggestion(suggestion)}
+                            value={suggestion}
+                          />
                         );
                       })}
-                    </div>
+                    </datalist>
                     <button type="submit" class="transition ease-in-out duration-200 text-white absolute right-2.5 bottom-2.5 bg-emerald-500 hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-semibold rounded-lg text-sm px-4 py-2 dark:bg-emerald-400 dark:hover:bg-emerald-600 dark:focus:ring-green-300">Search</button>
                   </div>
                 </form>
@@ -136,6 +139,20 @@ export default function SearchBar() {
                 </button>
               </div>
             </div>
+            {/*
+            <div className="text-emerald-400 dark:text-emerald-400 flex flex-col gap-2 max-w-screen-md ml-40 pr-9 w-7/12 self-start bg-gray-50">
+              {suggestions.map(suggestion => {
+                return(
+                  <div
+                    className="cursor-pointer"
+                    onClick={() => selectSuggestion(suggestion)}
+                  >
+                    {suggestion}
+                  </div>
+                );
+              })}
+            </div>
+            */}
           </div>
         </div>
     </>
