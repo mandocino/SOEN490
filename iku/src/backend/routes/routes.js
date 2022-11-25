7f28a2f2ad806fcce0061a32feb52aa9688f40d1
Tommy Soucy <soucy.tommy@gmail.com>
2022-11-25 16:28:24 -0500

    IKU-155 Actually added the route

--- iku/src/backend/routes/routes.js
+++ iku/src/backend/routes/routes.js
@@ -6,6 +6,7 @@ import { showUsers, showUserByID, showUserByEmail, loginController , signupContr
 import { addLocation, getAddressByCoordinates, showLocationsByUserID, getSuggestions, getCoordinatesByAddress, updateLocation, deleteLocation } from "../controllers/location.js";
 import { addEmailConfirmation, getEmailConfirmation, removeEmailConfirmation } from "../controllers/emailConfirmation.js";
 import { addPasswordResetRequest, getPasswordResetRequest, updatePasswordResetRequest, removePasswordResetRequest } from "../controllers/passwordResetRequest.js";
+import {showSavedScoresByLocations} from "../controllers/savedScore.js";
 
 // init express router
 const router = express.Router();
@@ -57,6 +58,11 @@ router.post('/updateLocation/', updateLocation);
 
 // Delete a location by object id
 router.post('/deleteLocation/', deleteLocation);
+
+///////////// SAVED SCORE
+
+// Get all saved scores from an origin to a destination
+router.get('/savedScores/:origin/:destination', showSavedScoresByLocations);
 
 ///////////// EMAIL CONFIRMATION
 
