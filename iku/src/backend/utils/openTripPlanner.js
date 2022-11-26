0a2c7a37df83aa59d91d09cad706e441062691b2
Ali Hanni <alihanni95@gmail.com>
2022-11-25 19:39:42 -0500

    IKU-110 Created getWalkWaitComponents function

---
+++ iku/src/backend/utils/openTripPlanner.js
@@ -0,0 +1,13 @@
+
+export const getWalkWaitComponents = (route) => {
+    return {
+        walk: {
+            time: 0,
+            components: []
+        },
+        wait: {
+            time: 0,
+            components: []
+        }
+    }
+}
