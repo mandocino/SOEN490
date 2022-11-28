894bcefb6fdb960247651c5f1959d7bbebab907c
AseelAce <aseelking2015@gmail.com>
2022-11-27 21:38:10 -0500

    IKU-113: Create function to load scores

--- iku/src/backend/utils/scoring.js
+++ iku/src/backend/utils/scoring.js
@@ -46,3 +46,39 @@ export function generateNewScores(origin, destination, user){
   };
   saveScores(origin, destination, scores, date);
 };
+
+export async function getScores(orgin, destination, user){
+    await axios.get(`http://localhost:5000/savedScores/${origin}/${destination}`, {
+        params:
+        {
+            origin:origin,
+            destination:destination,
+        }
+    }).then(async (response) => {
+        // We need to add the Algorithm update check and the Users preference last udate check
+        return response.data;
+
+    }).catch(err => console.log(err));
+};
+
+export async function loadScores(origin, destination, user){
+    var savedScores;
+    var timeValues = await axios.get('http://localhost:5000/global/');
+    var lastUpdateAlgoUpdateTime = timeValues.data[0].lastUpdateAlgoUpdateTime;
+    var user = await axios.get(`http://localhost:5000/userByID/${user}`);
+    var lastPrefChangeTime = user.data[0].lastPrefChangeTime;
+    savedScores = getScores(origin,destination,user);
+    if (savedScores==null || savedScores.date << lastPrefChangeTime || savedScores.date << lastUpdateAlgoUpdateTime ){
+        generateNewScores(origin,destination,user);
+        savedScores = getScores(origin,destination,user);
+    }
+    var scores = {
+        overall: savedScores.overall,
+        rushHour: savedScores.rushHour,
+        offPeak: savedScores.offPeak,
+        weekend: savedScores.weekend,
+        overNight: savedScores.overNight
+    }
+    console.log(scores);
+    return scores;
+};
