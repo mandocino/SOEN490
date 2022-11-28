47c92f38a3ef5045a6058a5d3e0f747533d05561
AseelAce <aseelking2015@gmail.com>
2022-11-27 21:37:24 -0500

    IKU-114: Create function to generate scores

--- iku/src/backend/utils/scoring.js
+++ iku/src/backend/utils/scoring.js
@@ -18,3 +18,31 @@ export async function saveScores(origin, destination, scores, date) {
    });
 };
 
+export function generateNewScores(origin, destination, user){
+  var rushHour = (Math.random()*100) + 1;
+  Math.floor(this.rushHour);
+  
+  var offPeak = (Math.random()*100) + 1;
+  Math.floor(this.offPeak);
+  
+  var weekend = (Math.random()*100) + 1;
+  Math.floor(this.weekend);
+  
+  var night = (Math.random()*100) + 1;
+  Math.floor(this.night);
+  
+  var overall = (Math.random()*100) + 1;
+  Math.floor(this.night);
+
+  var date = Date.now();
+
+  var scores = 
+  {
+    overall:overall,
+    rushHour:rushHour,
+    offPeak:offPeak,
+    weekend:weekend,
+    overNight:night
+  };
+  saveScores(origin, destination, scores, date);
+};
