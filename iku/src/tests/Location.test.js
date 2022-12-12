e477faa3bada9bfa0dc74aa88372bceafc89c008
Tommy Soucy <soucy.tommy@gmail.com>
2022-12-12 17:39:17 -0500

    IKU-129 Added Location create and get

--- iku/src/tests/Location.test.js
+++ iku/src/tests/Location.test.js
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 import axios from 'axios';
+import mongoose from "mongoose";
 
 jest.setTimeout(10000);
 
@@ -50,4 +51,32 @@ describe("Google Places Autocomplete API test", () => {
         expect(suggestionsResponse.status).toBe(200); // 200 --> successful request
         expect(suggestions[0]).toBe("Montreal, QC, Canada");
     })
+});
+
+describe("Database tests", () => {
+    test("Create", async () => {
+        const resSignup = await axios.post(`http://localhost:5000/newlocation/`, {
+            user_id: new mongoose.mongo.ObjectId('6334936ea7e4368f95ec50c9'),
+            latitude: 0,
+            longitude: 0,
+            name: "Test",
+            origin: true,
+            current_home: true
+        });
+        expect(resSignup.data).toHaveProperty('_id');
+    });
+
+    test("Get", async () => {
+        const objectID = new mongoose.mongo.ObjectId('6334936ea7e4368f95ec50c9');
+        const resGet = await axios.get(`http://localhost:5000/locations/${objectID}`);
+        expect(Array.isArray(resGet.data)).toBe(true);
+        expect(resGet.data.length).toBeGreaterThanOrEqual(1);
+        let found = false;
+        for(let i = 0;i<resGet.data.length;++i){
+            if(resGet.data[i].user_id == '6334936ea7e4368f95ec50c9' && resGet.data[i].name == 'Test'){
+                found = true;
+            }
+        }
+        expect(found).toBe(true);
+    });
 });
