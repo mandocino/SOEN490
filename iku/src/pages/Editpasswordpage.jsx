277a7635ef6c266df717cf513c5dbd4e306642fc
Cinodevv <96893418+Cinodevv@users.noreply.github.com>
2023-01-08 11:29:10 -0500

    Change Password Functionality
    Users can now change their passwords for the account

---
+++ iku/src/pages/Editpasswordpage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BaseLayout from "../components/BaseLayout";
+import EditPassword from "../components/Editpassword";
+
+export default function Editinfopage() {
+  return (
+    <>
+      <BaseLayout>
+        <EditPassword />
+      </BaseLayout>
+    </>
+  );
+}
