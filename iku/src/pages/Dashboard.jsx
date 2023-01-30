801b9baa72a245e4f541669026f30fe17e5d9b55
FrederickMunro <fredmunro7@gmail.com>
2023-01-30 00:47:39 -0500

    Refactoring

--- iku/src/pages/Dashboard.jsx
+++ iku/src/pages/Dashboard.jsx
@@ -9,8 +9,6 @@ import { ReactComponent as FrequencyIcon } from "./../assets/table-solid.svg";
 import { ReactComponent as WalkIcon } from "./../assets/person-walking-solid.svg";
 import {loadScores} from "../backend/utils/scoring";
 
-
-
 export default function Dashboard() {
 
   const user_id = localStorage.getItem("user_id");
@@ -171,7 +169,7 @@ export default function Dashboard() {
 
   return (
     <>
-      <BaseLayout className="flex flex-col">
+      <BaseLayout className="flex flex-col" ignore={['dashboard']}>
         {/*<div className="w-full grow flex flex-col items-center p-8 bg-cover bg-center bg-fixed bg-[url('/src/assets/dashboard_bg.jpg')]">*/}
         <div className="w-full grow flex flex-col items-center p-8">
           <div className="w-full flex flex-col justify-center">
