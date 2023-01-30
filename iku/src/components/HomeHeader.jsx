0a5b8d6037d17df96422207a5c0e98ad03605016
FrederickMunro <fredmunro7@gmail.com>
2023-01-29 21:48:46 -0500

    Rebase from main

--- iku/src/components/HomeHeader.jsx
+++ iku/src/components/HomeHeader.jsx
@@ -1,11 +1,17 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState, useRef } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import LinkButton from "./custom/LinkButton";
 import Button from "./custom/Button";
 import Logo from "./custom/Logo";
+import mongoose from "mongoose";
+import axios from "axios";
+
+import SimpleSearchBar from './SimpleSearchBar';
 
 export default function HomeHeader() {
+  
   const location = useLocation();
+
   return (
     <header>
       <nav className="bg-emerald-500 dark:bg-emerald-700 px-4 lg:px-6 py-2.5 ">
@@ -22,47 +28,8 @@ export default function HomeHeader() {
                 <LinkButton to="/dashboard">Dashboard</LinkButton>
               </li>
               {location.pathname !== "/" ? (
-                <li className="w-96">
-                  <form>
-                    <label
-                      htmlFor="default-search"
-                      className="mb-2 text-sm font-medium text-white sr-only"
-                    >
-                      Search
-                    </label>
-                    <div className="relative">
-                      <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                        <svg
-                          aria-hidden="true"
-                          className="w-5 h-5 text-emerald-100"
-                          fill="none"
-                          stroke="currentColor"
-                          viewBox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                          ></path>
-                        </svg>
-                      </div>
-                      <input
-                        type="search"
-                        id="default-search"
-                        className="transition ease-in-out duration-200 block p-4 pl-10 w-full text-md font-semibold text-white placeholder-white bg-emerald-500 rounded-lg border-2 border-emerald-200 dark:border-emerald-300 accent-white focus:border-white dark:bg-emerald-700 dark:placeholder-emerald-100"
-                        placeholder="Search Location"
-                        required
-                      />
-                      <button
-                        type="submit"
-                        className="transition ease-in-out duration-200 text-emerald-600 dark:text-emerald-800 absolute right-3 bottom-3 bg-emerald-200 hover:bg-white focus:ring-4 focus:outline-none focus:ring-green-300 font-semibold rounded-lg text-sm px-4 py-2 dark:bg-emerald-300 dark:focus:ring-green-300"
-                      >
-                        Search
-                      </button>
-                    </div>
-                  </form>
+                <li class="w-96">
+                  <SimpleSearchBar />
                 </li>
               ) : null}
               <li className="flex items-center">
---
+++ iku/src/components/SimpleSearchBar.jsx
@@ -0,0 +1,156 @@
+import { React, useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import mongoose from "mongoose";
+import axios from "axios";
+import { ReactComponent as Location } from "./../assets/location.svg";
+
+const SimpleSearchBar = () => {
+
+  const inputRef = useRef(null); // To autofill the textbox after fetching current location
+
+  const [input, setInput] = useState(""); // Handles input in the search bar
+
+  const [suggestions, setSuggestions] = useState([]); // Handles places suggestions returned by api
+
+  const navigate = useNavigate();
+
+  const selectSuggestion = (suggestion) => {
+    inputRef.current.value = suggestion;
+    setInput(suggestion);
+    setSuggestions([]);
+  };
+
+  const handleEnterPressed = async (event) => {
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await axios
+      .get("http://localhost:5000/coordinates", {
+        params: {
+          address: input,
+        },
+      })
+      .then(async (response) => {
+        const userId = localStorage.getItem("user_id");
+        const location = {
+          latitude: response.data.coordinates.lat,
+          longitude: response.data.coordinates.lng,
+          name: input.split(",")[0],
+          notes: input,
+          origin: true,
+          current_home: false,
+        };
+
+        let locationStringArray;
+        if(userId == null) {
+          locationStringArray = sessionStorage.getItem("location")
+
+          if(locationStringArray == null) {
+            sessionStorage.setItem('location', JSON.stringify([location]));
+          } else {
+            let locationsArray = JSON.parse(locationStringArray);
+            locationsArray.push(location);
+            sessionStorage.setItem('location', JSON.stringify(locationsArray));
+          }
+        } else {
+          await axios
+            .post("http://localhost:5000/newlocation", {
+              user_id: mongoose.Types.ObjectId(userId),
+              ...location
+            })
+          .catch((error) => {
+            console.log(error.message);
+          });
+        }
+        navigate("/dashboard");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  const getSuggestions = async (event) => {
+    setInput(event.target.value);
+    if (event.target.value.length > 0) {
+      await axios
+        .get("http://localhost:5000/suggestions", {
+          params: {
+            input: event.target.value,
+          },
+        })
+        .then((response) => {
+          setSuggestions(response.data.predictions);
+        })
+        .catch((error) => {
+          console.log(error.message);
+          setSuggestions([]);
+        });
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  return (
+    <>
+      <form class="rounded-lg drop-shadow-lg grow">
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">
+          Search
+        </label>
+        <div class="relative">
+          <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <svg
+                aria-hidden="true"
+                class="w-5 h-5 text-gray-500"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </div>
+          <input
+            ref={inputRef}
+            onChange={getSuggestions}
+            onKeyDown={handleEnterPressed}
+            type="search"
+            id="default-search"
+            class="accent-emerald-700 dark:accent-white transition ease-in-out duration-200 block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border-2 border-emerald-500 focus:border-emerald-700 dark:bg-gray-700 dark:border-emerald-400 dark:placeholder-gray-400 dark:text-white dark:focus:border-white"
+            placeholder="Search Location"
+            list="suggestions"
+            autocomplete="off"
+            required
+          />
+          <datalist id="suggestions">
+            {suggestions.map((suggestion) => {
+              return (
+                <option
+                  className="cursor-pointer"
+                  onClick={() => selectSuggestion(suggestion)}
+                  value={suggestion}
+                />
+              );
+            })}
+          </datalist>
+          <button
+            onClick={handleSubmit}
+            class="transition ease-in-out duration-200 text-white absolute right-2.5 bottom-2.5 bg-emerald-500 hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-semibold rounded-lg text-sm px-4 py-2 dark:bg-emerald-400 dark:hover:bg-emerald-600 dark:focus:ring-green-300"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default SimpleSearchBar
