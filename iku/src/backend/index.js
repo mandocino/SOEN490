a7c4b3ad5b72168a2a1d7abf30b979acd39596f2
Cinodevv <96893418+Cinodevv@users.noreply.github.com>
2023-03-16 22:36:22 -0400

    baseline test

--- iku/src/backend/index.js
+++ iku/src/backend/index.js
@@ -1,31 +1,20 @@
-import * as dotenv from 'dotenv' 
-dotenv.config()
-// import express
-import express from "express";
-// import cors
-import cors from "cors";
-// import routes
-import Router from "./routes/routes.js";
+// index.js
+const express = require('express')
 
-import {connectToServer} from "./config/db.js";
+const app = express()
+const PORT = 4000
 
-// init express
-const app = express();
-app.disable("x-powered-by");
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
 
-// use express json
-// with payload limit as 10mb
-app.use(express.json({limit: '10mb'}));
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
 
-// use cors
-app.use(cors());
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
 
-// use router
-app.use(Router);
-
-// Connect to DB
-connectToServer();
-
-app.listen(5000, () => console.log("Server running at http://localhost:5000"));
-
+// Export the Express API
 module.exports = app
