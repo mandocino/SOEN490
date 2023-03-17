a13474afcb9979f2c84c498947cac9f935840798
Cinodevv <96893418+Cinodevv@users.noreply.github.com>
2023-03-16 22:46:43 -0400

    more test

--- iku/src/backend/index.js
+++ iku/src/backend/index.js
@@ -1,20 +1,54 @@
-// index.js
-const express = require('express')
+import * as dotenv from 'dotenv' 
+dotenv.config()
+// import express
+import express from "express";
+// import cors
+import cors from "cors";
+// import routes
+import Router from "./routes/routes.js";
 
-const app = express()
-const PORT = 4000
+import {connectToServer} from "./config/db.js";
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+// init express
+const app = express();
+//app.disable("x-powered-by");
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+// use express json
+// with payload limit as 10mb
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
 
-// Export the Express API
+// Connect to DB
+//connectToServer();
+
+app.listen(4000, () => console.log("Server running at http://localhost:4000"));
+
+app.use(express.json({limit: '10mb'}));
+
+// use cors
+app.use(cors());
+
+// use router
+app.use(Router);
+
 module.exports = app
+
+// // index.js
+// const express = require('express')
+
+// const app = express()
+// const PORT = 4000
+
+// app.listen(PORT, () => {
+//   console.log(`API listening on PORT ${PORT} `)
+// })
+
+// app.get('/', (req, res) => {
+//   res.send('Hey this is my API running 🥳')
+// })
+
+// app.get('/about', (req, res) => {
+//   res.send('This is my about route..... ')
+// })
+
+// // Export the Express API
+// module.exports = app
